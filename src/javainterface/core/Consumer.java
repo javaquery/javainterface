f603e52f6887f0659418cfe27b270ec84137f278
javaquery <vicky.thakor@javaquery.com>
2016-09-01 08:28:38 +0530

    Producer - Consumer

---
+++ src/javainterface/core/Consumer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javainterface.core;
+
+/**
+ * @author javaQuery
+ * @date 1st September, 2016
+ * @Github: https://github.com/javaquery/javainterface
+ * @param <T>
+ */
+public interface Consumer<T> {
+    public T receive();
+}
